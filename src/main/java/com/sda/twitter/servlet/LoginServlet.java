8e87045767880cf5ecd5ffe407427663c84642ef
Magda Czyz <stajniaenklawa@gmail.com>
2020-01-25 10:42:17 +0100

    dadanie loginu

--- src/main/java/com/sda/twitter/servlet/LoginServlet.java
+++ src/main/java/com/sda/twitter/servlet/LoginServlet.java
@@ -1,13 +1,21 @@
 package com.sda.twitter.servlet;
 
+import com.sda.twitter.exception.ImproperLoginCredentials;
+import com.sda.twitter.persistance.entities.TbUser;
+import com.sda.twitter.service.UserService;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
+    private UserService userService = new UserService();
+
     //    @Override
 //    public void doGet(HttpServletRequest request
 //            , HttpServletResponse response
@@ -15,14 +23,19 @@ public class LoginServlet extends HttpServlet {
 //        doPost(request, response);
 //    }
     @Override
-    public void doPost(HttpServletRequest request
+    public void doPost(
+            HttpServletRequest request
             , HttpServletResponse response
     ) throws IOException {
         String userName = request.getParameter("username");
         String password = request.getParameter("password");
-        if (userName.equals("admin") && password.equals("password")) {
+        try {
+            TbUser tbUser = userService.getUserByUserName(userName, password);
+            HttpSession session = request.getSession();
+            session.setAttribute("currentUser", tbUser);
             response.sendRedirect("/");
-        } else {
+        } catch (ImproperLoginCredentials improperLoginCredentials) {
+            improperLoginCredentials.printStackTrace();
             response.sendRedirect("login.jsp");
         }
     }
