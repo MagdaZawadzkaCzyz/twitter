2231ab1db4eb044fa2b7bf38ed44e6d9dd2acb83
Magda Czyz <stajniaenklawa@gmail.com>
2020-01-26 11:09:09 +0100

    implementacjadodawania artykułu z bazy danych

--- src/main/java/com/sda/twitter/service/ArticleService.java
+++ src/main/java/com/sda/twitter/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.sda.twitter.service;
 
 import com.sda.twitter.persistance.dao.ArticleDao;
+import com.sda.twitter.persistance.entities.TbUser;
 
 import java.util.List;
 
@@ -8,7 +9,12 @@ public class ArticleService {
     private ArticleDao articleDao = new ArticleDao();
 
     public List getArticles() {
+
         return articleDao.getArticles();
+    }
+
+    public void addArticle(TbUser tbUser, String content) {
+        articleDao.addArticle(tbUser, content);
     }
 
 }
